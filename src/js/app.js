fec8cf1bd05daf08fa97606686e30527862b8845
Чернова Александра <estelelendil@mail.ru>
2022-07-11 22:13:05 +0300

    Webpuck install and worked

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import { Game, GameSavingData, loadGame, saveGame} from './game.js';
+
+console.log('app worked');
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
